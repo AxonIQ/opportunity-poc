f6cd1a4db07503c3bbc59ddb8a3ad8983795ac5f
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-02-27 15:49:57 +0100

    Add SpringBootApplication with main method + test
    Add SpringBootApplication with main method + test
    
    #init

---
+++ src/main/java/io/axoniq/opportunity/OpportunityPocApplication.java
@@ -0,0 +1,13 @@
+package io.axoniq.opportunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpportunityPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpportunityPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/axoniq/opportunity/OpportunityPocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.axoniq.opportunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpportunityPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
