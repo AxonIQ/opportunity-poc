55efd58cb964e56b297f87e3d1a701461c33d8c7
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-02-27 15:56:08 +0100

    Add distinct identifier classes so that the ids don't clash
    Add distinct identifier classes so that the ids don't clash
    
    #init

---
+++ src/main/java/io/axoniq/opportunity/coreapi/AccountId.java
@@ -0,0 +1,39 @@
+package io.axoniq.opportunity.coreapi;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class AccountId {
+
+    private final UUID identifier;
+
+    public AccountId(UUID identifier) {
+        this.identifier = identifier;
+    }
+
+    public UUID getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AccountId accountId = (AccountId) o;
+        return Objects.equals(identifier, accountId.identifier);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identifier);
+    }
+
+    @Override
+    public String toString() {
+        return identifier.toString();
+    }
+}
---
+++ src/main/java/io/axoniq/opportunity/coreapi/OpportunityId.java
@@ -0,0 +1,39 @@
+package io.axoniq.opportunity.coreapi;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class OpportunityId {
+
+    private final UUID identifier;
+
+    public OpportunityId(UUID identifier) {
+        this.identifier = identifier;
+    }
+
+    public UUID getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OpportunityId accountId = (OpportunityId) o;
+        return Objects.equals(identifier, accountId.identifier);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identifier);
+    }
+
+    @Override
+    public String toString() {
+        return identifier.toString();
+    }
+}
