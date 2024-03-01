345ab638eefd5076d979e8954c1f5423556ccb9d
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-03-01 09:29:23 +0100

    Deal with basic opportunity events in opportunity projector
    Deal with basic opportunity events in opportunity projector
    
    #init

--- src/main/java/io/axoniq/opportunity/query/opportunity/OpportunityProjector.java
+++ src/main/java/io/axoniq/opportunity/query/opportunity/OpportunityProjector.java
@@ -1,11 +1,13 @@
 package io.axoniq.opportunity.query.opportunity;
 
+import io.axoniq.opportunity.coreapi.opportunity.OpportunityOpenedEvent;
+import io.axoniq.opportunity.coreapi.opportunity.OpportunityStageChangedEvent;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
 @Component
-@ProcessingGroup("opportunity-projector")
+@ProcessingGroup("opportunity")
 class OpportunityProjector {
 
     private final OpportunityRepository repository;
@@ -15,7 +17,19 @@ class OpportunityProjector {
     }
 
     @EventHandler
-    public void on(String event) {
+    public void on(OpportunityOpenedEvent event) {
+        repository.save(new OpportunityView(event.getOpportunityId().toString(),
+                                            event.getAccountId().toString(),
+                                            event.getName(),
+                                            event.getEndDate()));
+    }
 
+    @EventHandler
+    public void on(OpportunityStageChangedEvent event) {
+        repository.findById(event.getOpportunityId().toString())
+                  .map(opportunityView -> {
+                      opportunityView.setStage(event.getStage());
+                      return opportunityView;
+                  });
     }
 }
