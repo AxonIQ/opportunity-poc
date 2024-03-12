c93dcc72f1e6efaff037b5ce1fe8f482c59548d7
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-03-12 17:39:00 +0100

    Remove resolved TODO
    Remove resolved TODO
    
    #finalize

--- src/main/java/io/axoniq/opportunity/coreapi/opportunity/quote/RejectQuoteCommand.java
+++ src/main/java/io/axoniq/opportunity/coreapi/opportunity/quote/RejectQuoteCommand.java
@@ -3,7 +3,6 @@ package io.axoniq.opportunity.coreapi.opportunity.quote;
 import io.axoniq.opportunity.coreapi.opportunity.OpportunityId;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
-// TODO Use command for testing in the UI
 public record RejectQuoteCommand(@TargetAggregateIdentifier OpportunityId opportunityId, QuoteId quoteId) {
 
 }
